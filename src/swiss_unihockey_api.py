99789c71e5a5efaf6018522f4e597d7b27e62a72
rufer7 <rufermarc@gmail.com>
2020-12-19 00:05:54 +0100

    swiss_unihockey_api.py - skeleton class added for game record

---
+++ src/swiss_unihockey_api.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*- #
+
+"""
+
+wrapper for swiss unihockey API v2
+
+
+"""
+from dataclasses import dataclass
+
+
+@dataclass
+class GameRecord:  # pylint: disable=too-few-public-methods
+    """
+    game record
+    """
