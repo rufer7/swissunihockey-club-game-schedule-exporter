dd8894633ff9d5e323ca565a02dc496cfdeebe5b
rufer7 <rufermarc@gmail.com>
2020-12-19 00:03:02 +0100

    export_club_game_schedule.py - skeleton implemented

--- src/export_club_game_schedule.py
+++ src/export_club_game_schedule.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
+# -*- coding: utf-8 -*- #
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+"""
+
+script for exporting the game schedule of the selected club to a word document
+
+input: data from swiss unihockey API v2
+output: word document
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+"""
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def main() -> None:
+    """
+    main entry point
+    """
+    # TODO - execute function
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+if __name__ == "__main__":
+    main()
