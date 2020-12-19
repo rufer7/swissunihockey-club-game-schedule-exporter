f1a972c99ce09c1eab9b9b489c59b0a2ae2f0431
rufer7 <rufermarc@gmail.com>
2020-12-19 22:47:35 +0100

    export_club_game_schedule.py - table creation implemented

--- src/export_club_game_schedule.py
+++ src/export_club_game_schedule.py
@@ -9,13 +9,50 @@ output: word document
 
 
 """
+import docx
+
+from docx_util import add_report_table
+from swiss_unihockey_api import GameRecord
+
+
+def insert_games(document: docx.Document, games: list[GameRecord]) -> None:
+    """
+    insert table containing games into document
+    @param document:
+    @param games:
+    @return: nothing
+    """
+    table = add_report_table(document, ('Datum', 'Anpfiff', 'Hornets-Team', 'Gegner'), len(games) + 1)
+    for i, game in enumerate(games):
+        row = i + 1
+        table.cell(row, 0).text = game.date
+        table.cell(row, 1).text = game.start_time
+        table.cell(row, 2).text = game.team_name
+        table.cell(row, 3).text = game.opponent
+
+
+def generate_game_schedule(document: docx.Document) -> None:
+    """
+    generate game schedule
+    @param document:
+    @return: nothing
+    """
+    games: list[GameRecord] = []
+    insert_games(document, games)
+    # TODO - insert paragraph
+    # Alle aufgeführten Spiele finden in der Raiffeisen unihockeyARENA am Sportweg 3 in 3322 Urtenen-Schönbühl statt!
+    # Wir freuen uns, Sie in unserer Halle als Zuschauer begrüssen zu dürfen.
+    #
+    # Hornets Regio Moosseedorf Worblental
 
 
 def main() -> None:
     """
     main entry point
     """
-    # TODO - execute function
+    document = docx.Document("empty-game-schedule.docx")
+    generate_game_schedule(document=document)
+    document.save("game-schedule.docx")
 
 
 if __name__ == "__main__":
--- src/swiss_unihockey_api.py
+++ src/swiss_unihockey_api.py
@@ -7,6 +7,7 @@ wrapper for swiss unihockey API v2
 
 """
 from dataclasses import dataclass
+from datetime import date
 
 
 @dataclass
@@ -14,3 +15,7 @@ class GameRecord:  # pylint: disable=too-few-public-methods
     """
     game record
     """
+    date: date = None
+    start_time: str = ""
+    team_name: str = ""
+    opponent: str = ""
